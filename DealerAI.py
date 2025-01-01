b08a9e6f6605b48020acd5bf0623babf25ed5ad0
Stuart Gilbert <stuartrgilbert@outlook.com>
2024-12-31 20:54:26 -0600

    Tesing..

--- DealerAI.py
+++ DealerAI.py
@@ -1,1 +1,3 @@
-
+class DealerAI:
+    def __init__(self, game_state):
+        pass
--- Deck.py
+++ Deck.py
@@ -1,29 +1,44 @@
 from typing import List
 import Card
+import random
 
 class Deck:
-    def __init__(self, cards: List = []):
+    def __init__(self, cards: List = []) -> None:
         self.cards = cards
-        pass
+        return
+
+    # TODO change constructor to accept an integer which will represent the number of default decks that will form the deck.
+    # i.e., if a 3 is given, then the deck will contain 3 default decks
 
     def makeDefaultDeck(self) -> None: 
         suits = ["SPADES", "HEARTS", "DIAMONDS", "CLUBS"]
         values = ["ACE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE", "TEN", 
                   "JACK", "QUEEN", "KING"]
-        _cards = []
-        suitCount = 0
-        valueCount = 0
+        deck = []
 
-        for i in range(0, 52):
-            _cards.append(Card(suits[suitCount], values[valueCount]))
-            suitCount += 1
-            valueCount += 1
+        for i in suits:
+            for j in values:
+                deck.append(Card.Card(i, j))
 
-            if suitCount == 4:
-                suitCount = 0
-            if valueCount == 13:
-                valueCount = 0
-        self.cards = _cards
+        self.cards = deck
+        return
+
+    def shuffle(self) -> None:
+        self.cards = random.sample(self.cards, len(self.cards))
+
+    def toString(self) -> str:
+        string = ""
+        count = 1
+
+        for card in self.cards:
+            string += str(count) + " " + card.toString() + "\n"
+            count += 1
+
+        return string
+
+    def dealCard(self) -> Card:
+        return self.cards.pop()
+   
 
 
 
