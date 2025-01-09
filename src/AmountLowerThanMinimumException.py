03fceb7ba9d62eb07e3c7d426d8e06ddec9d6fc4
Stuart Gilbert <stuartrgilbert@outlook.com>
2025-01-08 18:16:10 -0600

    Updated constructor type hints.

--- src/AmountLowerThanMinimumException.py
+++ src/AmountLowerThanMinimumException.py
@@ -1,5 +1,5 @@
 from src.CustomException import *
 
 class AmountLowerThanMinimumException(CustomException):
-    def __init__(self, message = ""):
+    def __init__(self, message = "") -> None:
         super().__init__(message)
--- src/CustomException.py
+++ src/CustomException.py
@@ -1,3 +1,3 @@
 class CustomException(BaseException):
-	def __init__(self, message = "Default"):
+	def __init__(self, message = "Default") -> None:
 		self.message = message
